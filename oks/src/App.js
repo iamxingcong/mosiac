c4ae697219d4653807eeb968ea6095cd4bdb4c34
xc <121950263@qq.com>
2021-07-10 03:19:51 +0800

    udpate

---
+++ oks/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Menu from './view/Menu.js'
+
+import Gs from './view/Gs.js'
+
+function App() {
+
+    return (
+
+        <div className="App">
+ 
+             <Menu  />
+
+            <Gs />
+          </div>
+
+    );
+
+
+}
+
+export default App;
---
+++ oks/src/view/Menu.js
@@ -0,0 +1,33 @@
+ import React from 'react';
+ import { Get } from 'react-axios'
+
+ function Menu() {
+
+     return (
+         <div className="App">
+    <Get url="http://192.168.1.14/wp/wordpress//wp-json/wp/v2/categories">
+     
+        {(error, response, isLoading, makeRequest, axios) => {
+          if(error) {
+            return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
+          }
+          else if(isLoading) {
+            return (<div>Loading...</div>)
+          }
+          else if(response !== null) {
+            return (<>
+              {response.data.map((user,index) =>
+                  <h1 key={index}>{user.name}</h1>
+              )}
+            </>)
+          }
+          return (<div>Default message before request is made.</div>)
+        }}
+      </Get>
+  </div>
+     );
+ }
+
+
+
+ export default Menu;
